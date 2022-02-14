d76cd315cf8724a555984a808af897a5d4827f5e
Minh Chinh <chinhtm90@gmail.com>
2022-02-14 16:42:43 +0700

    decode

--- hmac-sha256.py
+++ hmac-sha256.py
@@ -73,7 +73,7 @@ class Crypticle(object):
         print(self.PICKLE_PAD)
         print(type(self.PICKLE_PAD))
         # simple integrity check to verify that we got meaningful data
-        assert data.startswith(self.PICKLE_PAD), "unexpected header"
+        assert data.decode().startswith(self.PICKLE_PAD), "unexpected header"
         return pickler.loads(data[len(self.PICKLE_PAD):])
 
 
